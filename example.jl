bedfb2435ab4a190e8e2efc06d9bd493b18c4d85
Jake Rosoman <jkroso@gmail.com>
2015-06-26 16:35:31 +1200

    tidy

--- example.jl
+++ example.jl
@@ -5,12 +5,6 @@
 import Rutherford: rutherford
 import Patchwork: Elem
 
-options = [:width => 1200,
-           :height => 700,
-           :frame => true,
-           :console => true,
-           :title => "Rutherford Example"]
-
 guiˢ = @task begin
   produce(Elem(:p, "Loading"))
   for event in eventˢ
@@ -18,7 +12,13 @@ guiˢ = @task begin
   end
 end
 
-eventˢ,renderer = rutherford(guiˢ, options)
+eventˢ,renderer = rutherford(guiˢ, [
+  :width => 1200,
+  :height => 700,
+  :frame => true,
+  :console => true,
+  :title => "Rutherford Example"
+])
 
 # Wait on render task so errors are shown
 wait(renderer)
