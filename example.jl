c48a248a3618698b322b0d344f4c089a4499fc81
Jake Rosoman <jkroso@gmail.com>
2016-10-26 14:36:49 +1300

    styling

--- example.jl
+++ example.jl
@@ -2,7 +2,7 @@
 # This example just renders the most resent user event type as
 # a string. It demonstrates the round trip latency of the system
 #
-@require "github.com/jkroso/DOM.jl/stylesheet" global_sheet @css_str
+@require "github.com/jkroso/DOM.jl/stylesheet" global_sheet reset @css_str
 @require "github.com/jkroso/DOM.jl" @dom
 @require "." App Window
 
@@ -12,13 +12,18 @@ window = Window(app, Dict(:width => 1200,
                           :height => 700,
                           :titleBarStyle => :hidden))
 
-put!(window.ui, @dom [:html [:head global_sheet] [:body [:p "Loading"]]])
+class = css"""
+  display: flex
+  justify-content: space-around
+  align-items: center
+"""
+
+const head = @dom [:head global_sheet reset]
+
+put!(window.ui, @dom [:html head [:body class=class [:pre "Loading"]]])
 
 for e in window.events
-  put!(window.ui, @dom [:html
-    [:head global_sheet]
-    [:body [:pre class=css"text-align: center" repr(e)]]
-  ])
+  put!(window.ui, @dom [:html head [:body class=class [:pre repr(e)]]])
 end
 
 wait(app) # keeps the process open
