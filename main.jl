985588daa79e7a5fb03664008263b13f3a0d5a7b
Jake Rosoman <jkroso@gmail.com>
2016-10-28 16:22:58 +1300

    define dispatch(::Window,::Event)

--- main.jl
+++ main.jl
@@ -1,5 +1,4 @@
-@require "github.com/jkroso/DOM.jl/Events" => Events
-@require "github.com/jkroso/DOM.jl" diff runtime
+@require "github.com/jkroso/DOM.jl" diff runtime Events dispatch Node
 @require "github.com/jkroso/Electron.jl" install
 @require "github.com/jkroso/write-json.jl"
 @require "github.com/jkroso/parse-json.jl"
@@ -12,6 +11,8 @@ type Window
   ui::Port
   events::Task
   renderLoop::Task
+  currentUI::Node
+  Window() = new(Port())
 end
 
 type App
@@ -20,47 +21,49 @@ type App
   proc::Base.Process
 end
 
-App(title; version=v"1.4.4") = App(title, open(`$(install(version)) $app_path`, "w")...)
+App(title; version=v"1.4.4") = App(title, open( `$(install(version)) $app_path`, "w")...)
 
 Window(a::App, params::Associative) = begin
+  window = Window()
   port,server = listenany(3000)
   params = Dict(:title=>a.title, params..., :query=>Dict(:port=>port, :runtime=>runtime))
-  ui_port = Port()
   show(a.stdin, json_mime, params)
   write(a.stdin, '\n')
 
   # connect with the window
   sock = accept(server)
 
-  renderLoop = @schedule try
-    currentGUI = take!(ui_port)
+  window.renderLoop = @schedule try
+    window.currentUI = take!(window.ui)
 
     # Send over initial rendering
-    show(sock, json_mime, currentGUI)
+    show(sock, json_mime, window.currentUI)
     write(sock, '\n')
 
     # Write patches
-    while isopen(ui_port)
-      nextGUI = take!(ui_port)
-      patch = diff(currentGUI, nextGUI)
+    while isopen(window.ui)
+      nextGUI = take!(window.ui)
+      patch = diff(window.currentUI, nextGUI)
       isnull(patch) && continue
       show(sock, json_mime, patch)
       write(sock, '\n')
-      currentGUI = nextGUI
+      window.currentUI = nextGUI
     end
   finally
     close(server)
   end
 
   # Produce a series of events
-  output = @task for line in eachline(sock)
+  window.events = @task for line in eachline(sock)
     produce(Events.parse_event(line))
   end
 
   sleep(0) # allow tasks to get started
-  Window(ui_port, output, renderLoop)
+  return window
 end
 
 Base.wait(w::Window) = wait(w.renderLoop)
 Base.wait(a::App) = wait(a.proc)
 Base.put!(w::Window, n) = put!(w.ui, n)
+
+dispatch(w::Window, e::Events.Event) = dispatch(w.currentUI, e)
