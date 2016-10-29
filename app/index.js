264f8f3266b2c0ccc3f3c101076da51b21a271cc
Jake Rosoman <jkroso@gmail.com>
2016-10-29 20:42:00 +1300

    use write_event from DOM.jl

--- app/index.js
+++ app/index.js
@@ -19,74 +19,9 @@ const io = readline.createInterface({
 
 // write an event to the output stream
 const send = (event) => {
-  if (event.type in event_writers) {
-    event_writers[event.type](event)
-    sock.write('\n')
-  } else {
-    console.log(event.type + ' is not implemented')
-  }
+  runtime.write_event(sock, event)
   event.stopPropagation()
   event.preventDefault()
-}
-
-const write_modifiers = (event) => {
-  if (event.altKey) sock.write(' alt')
-  if (event.ctrlKey) sock.write(' ctrl')
-  if (event.shiftKey) sock.write(' shift')
-  if (event.metaKey) sock.write(' meta')
-}
-
-const write_key_event = (e, type) => {
-  sock.write(type + ' [' + dom_path(e.target) + '] ' + e.key)
-  write_modifiers(e)
-}
-
-const write_button_event = (e, type) => {
-  sock.write(type + ' [' + dom_path(e.target) + '] ' + e.button + ' ' + e.x + ' ' + e.y)
-}
-
-const top_node = document.lastElementChild
-
-const dom_path = (dom) => {
-  const indices = []
-  while (dom !== top_node) {
-    indices.push(indexOf(dom))
-    dom = dom.parentNode
-  }
-  return indices
-}
-
-const indexOf = (dom) => {
-  var i = 1 // julia indexes from 1
-  while (dom.previousSibling) {
-    dom = dom.previousSibling
-    i += 1
-  }
-  return i
-}
-
-const write_focus_change = (e, type) => {
-  if (e.target === window) {
-    sock.write(type + ' []')
-  } else {
-    sock.write(type + ' [' + dom_path(e.target) + ']')
-  }
-}
-
-const event_writers = {
-  keydown(e) { write_key_event(e, 'KeyDown') },
-  keyup(e) { write_key_event(e, 'KeyUp') },
-  mousemove(e) { sock.write('MouseMove ' + e.x + ' ' + e.y) },
-  mouseup(e) { write_button_event(e, 'MouseUp') },
-  mousedown(e) { write_button_event(e, 'MouseDown') },
-  mouseover(e) { sock.write('MouseOver [' + dom_path(e.target) + ']') },
-  mouseout(e) { sock.write('MouseOut [' + dom_path(e.target) + ']') },
-  click(e) { write_button_event(e, 'Click') },
-  dblclick(e) { write_button_event(e, 'DoubleClick') },
-  focus(e) { write_focus_change(e, 'Focus') },
-  blur(e) { write_focus_change(e, 'Blur') },
-  resize() { sock.write('Resize ' + window.innerWidth + ' ' + window.innerHeight) },
-  scroll(e) { sock.write('Scroll [' + dom_path(e.target) + '] ' + window.scrollX + ' ' + window.scrollY) },
 }
 
 // Listen to all events
