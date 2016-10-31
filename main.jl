5e4338644a309ce43ffcb24a9fe19af7a06b4cc1
Jake Rosoman <jkroso@gmail.com>
2016-10-31 21:27:32 +1300

    tidy

--- main.jl
+++ main.jl
@@ -2,19 +2,17 @@
 @require "github.com/jkroso/Electron.jl" install
 @require "github.com/jkroso/Cursor.jl" Cursor
 @require "github.com/jkroso/write-json.jl"
-@require "github.com/jkroso/parse-json.jl"
 @require "github.com/jkroso/Port.jl" Port
 
 const app_path = joinpath(@dirname(), "app")
 const json_mime = MIME("application/json")
 
 type Window
-  ready::Condition
   events::Port
   data::Port
   renderLoop::Task
   currentUI::Node
-  Window() = new(Condition(), Port(), Port())
+  Window() = new(Port(), Port())
 end
 
 type App
@@ -41,7 +39,6 @@ Window(a::App, data; kwargs...) = begin
     window.currentUI = convert(Container{:html}, Cursor(data, window.data))
     show(sock, json_mime, window.currentUI)
     write(sock, '\n')
-    notify(window.ready)
 
     # Write patches
     for cursor in window.data
@@ -56,15 +53,11 @@ Window(a::App, data; kwargs...) = begin
     close(server)
   end
 
-
   # Produce a series of events
-  @schedule begin
-    wait(window.ready)
-    for line in eachline(sock)
-      e = Events.parse_event(line)
-      dispatch(window, e)
-      put!(window.events, e)
-    end
+  @schedule for line in eachline(sock)
+    e = Events.parse_event(line)
+    dispatch(window, e)
+    put!(window.events, e)
   end
 
   return window
