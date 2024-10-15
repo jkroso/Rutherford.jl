03aba6a4986e3a658cddb2c7bd0fa3648fd522e7
Jake Rosoman <jkroso@gmail.com>
2024-10-15 20:31:45 +1100

    progress

--- stdlib/Table.jl
+++ stdlib/Table.jl
@@ -2,15 +2,25 @@
 
 table(attrs, children) = begin
   @dom[:div{css"""
-            border-radius: 0.6rem
+            border-radius: 3px
             background: white
-            border: 1px solid #e5e7eb
+            border: 1px solid #D0D0D0
             th {padding: 0.75rem 1.5rem; font: 1.3rem bolder, verdana}
             th,td {padding: 0.75rem 1.5rem}
             :where(tbody, thead) > tr > th {font: 1.3rem bolder, verdana}
             table
               width: 100%
               text-align: left
+              position: relative
+              overflow: auto
+              tbody tr {border-bottom: 1px solid #D0D0D0}
+              tbody tr:last-child {border-bottom: none}
+              tfoot
+                border-top: 1px solid #D0D0D0
+                tr > th {text-align: right}
+                tr > td {text-align: right}
+                tr:not(:last-child) > th {font-weight: lighter; font-family: monospace; color: rgb(130,130,130)}
+                tr:last-child > th {font-weight: bolder; font-family: monospace}
             """, attrs...}
     [:table children...]]
 end
@@ -22,18 +32,10 @@ header(attrs, children) =
     @dom[:thead{attrs...} [:tr (@dom[:th x] for x in children)...]]
   end
 body(attrs, children) = @dom[:tbody{attrs...} (get(attrs,:index,true) ? map(add_index, enumerate(children)) : children)...]
-add_index((i, tr)::Tuple{Number,Container{:tr}}) = @dom[:tr{tr.attrs...} [:th string(i)] tr.children...]
-footer(attrs, children) = begin
-  @dom[:tfoot{css"""
-              border-top: 1px solid #e5e7eb
-              tr > th {text-align: right}
-              tr > td {text-align: right}
-              tr:not(:last-child) > th {font-weight: lighter; font-family: monospace; color: rgb(130,130,130)}
-              tr:last-child > th {font-weight: bolder; font-family: monospace}
-              """, attrs...} children...]
-end
+add_index((i, tr)::Tuple{Number,Container}) = @dom[:tr{tr.attrs...} [:th string(i)] tr.children...]
+footer(attrs, children) = @dom[:tfoot{attrs...} children...]
 
-row(attrs, children) = @dom[:tr map(td, children)...]
+row(attrs, children) = @dom[:tr{attrs...} map(td, children)...]
 td(x::Union{Container{:td}, Container{:th}}) = x
 td(x::String) = @dom[:td x]
 td(x::Node) = @dom[:td x]
@@ -57,4 +59,4 @@ td(x::Node) = @dom[:td x]
   [body
     [row "Apple MacBook Pro 17" "Silver" "Laptop" "\$2999"]
     [row "Microsoft Surface Pro"	"White"	"Laptop PC"	"\$1999"]
-    [row "Magic Mouse 2"	"Black"	"Accessories"	"\$99"]]]
+    [row css"background: #D0D0D0" "Magic Mouse 2"	"Black"	"Accessories"	"\$99"]]]
